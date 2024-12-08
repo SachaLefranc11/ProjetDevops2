2533eddc8019b3dc7415b909dc97cbee9e97958d
sacha041211 <lefrancsacha1112@gmail.com>
2024-12-08 22:04:00 +0100

    update gitignore

--- models/db_conf.js
+++ models/db_conf.js
@@ -1,6 +1,7 @@
 // file db_conf.js in models
 
-const db = require('better-sqlite3')('/opt/web1_23-24/db/exoplanets.db', { verbose: console.log });
+const db = require('better-sqlite3')('C:/BLOC3/devops/ProjetDevops2/db/exoplanets.db', { verbose: console.log });
+
 // const db = require('better-sqlite3')('/home/olivier/exoplanets.db', { verbose: console.log });
 
 module.exports = db;
