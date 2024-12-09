148d908b7072e51a6209ebd138947e4d5881ebbf
sacha041211 <lefrancsacha1112@gmail.com>
2024-12-09 14:00:30 +0100

    add: Test for add new planet if does not exist

---
+++ spec/solarSystem.spec.js
@@ -0,0 +1,8 @@
+const SolarSystem = require('../models/SolarSystem'); // Exemple de modèle
+
+describe('Solar System Model', () => {
+  it('should add a new planet if it does not exist', () => {
+    const result = SolarSystem.addPlanet({ name: 'Mars', size: 6779, atmosphere: 'CO2', type: 'terrestrial' });
+    expect(result).toBeTruthy();
+  });
+});
